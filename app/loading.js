8be304ccba11c48e9864295a04425702d5317121
Jol0o <jloyd9836@gmail.com>
2023-09-04 14:28:51 +0800

    Update loading.js

--- app/loading.js
+++ app/loading.js
@@ -2,16 +2,22 @@
 import React from 'react'
 import { NewtonsCradle } from '@uiball/loaders'
 import { Flex } from '@mantine/core'
+import { useRouter } from 'next/navigation'
 
 
 function loading() {
+    const router = useRouter()
+    const refresh = () => {
+        router.refresh()
+    }
     return (
-        <Flex style={{ display: "flex", alignItems: "center", justifyContent: 'center', minHeight: '100vh', background: "#040D12" }}>
+        <Flex style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: 'center', minHeight: '100vh', background: "#040D12" }}>
             <NewtonsCradle
-                size={40}
+                size={80}
                 speed={1.4}
                 color="gray"
             />
+            <button onClick={refresh} style={{ background: "none", fontSize: "15px", fontWeight: "bold", textTransform: "uppercase", color: "gray", border: "none" }}>Refresh</button>
         </Flex>
     )
 }
